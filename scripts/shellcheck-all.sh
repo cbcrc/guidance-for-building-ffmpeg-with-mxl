ae61eb6b2795ee4e3c3b41cfa060bbcb4499e789
jpt <jim.trainor@jptrainor.com>
2026-01-30 16:29:05 -0500

    docs

--- scripts/shellcheck-all.sh
+++ scripts/shellcheck-all.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Run ShellCheck on project scripts.
+#
+# Install `shellcheck` first: "apt install shellcheck"
+#
+# See: https://www.shellcheck.net
+
 set -euo pipefail
 
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
