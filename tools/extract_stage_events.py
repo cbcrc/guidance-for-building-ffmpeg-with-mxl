228095e8a98fddbafe39df0518276f331e3aea5b
Jim Trainor <jim.trainor@jptrainor.com>
2026-03-05 15:18:56 -0500

    Update example command to use debug_ts.log

--- tools/extract_stage_events.py
+++ tools/extract_stage_events.py
@@ -7,10 +7,10 @@
 #
 # $ ffmpeg -loglevel debug -debug_ts ... > debug_ts.log 2>&1
 #
-# $ ./extract_stage_events.py mux "[vost#0:0/h264_nvenc @ 0x5da879b2f440]" /dev/shm/mxl/test-rtsp-ffmpeg.log 15 15  --summary
+# $ ./extract_stage_events.py mux "[vost#0:0/h264_nvenc @ 0x5da879b2f440]" debug_ts.log 15 15  --summary
 # median=4.478 p99=5.162 max=7.089 ms
 #
-# $ ./extract_stage_events.py mux "[vost#0:0/h264_nvenc @ 0x5da879b2f440]" /dev/shm/mxl/test-rtsp-ffmpeg.log 15 15  
+# $ ./extract_stage_events.py mux "[vost#0:0/h264_nvenc @ 0x5da879b2f440]" debug_ts.log 15 15  
 # count: 289
 # median: 4.478 ms
 # p99: 5.162 ms
