4fda174c733a104c8add37128f5b541880646762
jpt <jim.trainor@jptrainor.com>
2026-03-11 17:52:30 -0400

    update commit has for URI support

--- scripts/get-src.sh
+++ scripts/get-src.sh
@@ -59,7 +59,7 @@ clone_ffmpeg_repo() {
     git clone --single-branch --branch dmf-mxl/master --depth 1 https://github.com/cbcrc/FFmpeg.git
 
     cd FFmpeg
-    git switch --detach 914a712
+    git switch --detach 61d28a6
 }
 
 clone_x264_repo() {
