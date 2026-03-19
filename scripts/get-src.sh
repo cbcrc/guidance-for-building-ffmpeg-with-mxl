45d4550ebff4e73aeb33b3927811fea5a2e24ad3
jpt <jim.trainor@jptrainor.com>
2026-03-19 17:16:18 -0400

    update FFmpeg has to match mxl v1.0.0 error codes

--- scripts/get-src.sh
+++ scripts/get-src.sh
@@ -59,7 +59,7 @@ clone_ffmpeg_repo() {
     git clone --single-branch --branch dmf-mxl/master --depth 1 https://github.com/cbcrc/FFmpeg.git
 
     cd FFmpeg
-    git switch --detach fb9f016
+    git switch --detach 17ad10c
 }
 
 clone_x264_repo() {
