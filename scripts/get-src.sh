c4df610ff5b193793826e8de5461c2d72351caa9
jpt <jim.trainor@jptrainor.com>
2026-04-15 13:12:00 -0400

    update ffmpeg commit hash to get exec_dur diag msg

--- scripts/get-src.sh
+++ scripts/get-src.sh
@@ -71,7 +71,7 @@ clone_ffmpeg_repo() {
     git clone --single-branch --branch dmf-mxl/master --depth 1 https://github.com/cbcrc/FFmpeg.git
 
     cd FFmpeg
-    git switch --detach 4ea3355
+    git switch --detach ed6e484
 }
 
 clone_x264_repo() {
